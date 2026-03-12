94c1aea9e9aa788cd507f448c60de2c28ff04d18
Refinaldii <refinaldi9786@gmail.com>
2026-03-12 14:14:27 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Biodata
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white p-6 shadow rounded">
+                <h3 class="text-lg font-bold mb-4">Data Diri</h3>
+                <p><strong>Nama:</strong> Muhammad Refinaldi Alif</p>
+                <p><strong>NIM:</strong> 20230140200 </p>
+                <p><strong>Program Studi:</strong> Teknologi Informasi</p>
+                <p><strong>Hobi:</strong> Tidur</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
